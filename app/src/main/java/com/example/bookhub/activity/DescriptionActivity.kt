bed73c461a45e6717d7f7e748b6a555a0535a326
Kunal Pal <48083550+flashkp@users.noreply.github.com>
2020-07-02 12:42:50 +0530

    activity 2
    Signed-off-by: Kunal Pal <48083550+flashkp@users.noreply.github.com>

---
+++ app/src/main/java/com/example/bookhub/activity/DescriptionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookhub.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookhub.R
+
+class DescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_description)
+    }
+}
